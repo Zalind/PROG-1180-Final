571a5e778d4ff05c8f40b798780107b0985d1299
Zalind <34323350+Zalind@users.noreply.github.com>
2017-12-20 15:53:58 -0500

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function()
-{
-	$("#Navigation > ul li").click(function(){
-		if($("#Navigation > ul li").removeClass("active")){
-			$(this).removeClass("active");
-		}
-		$(this).addClass("active");
-	});
-});
